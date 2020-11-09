c3b4cc68921c6ff253a03acf590d06a004226e11
Joséphine Venisse <josephine.venisse@eivp-paris.fr>
2020-11-09 12:16:42 +0100

    1ere fonction détaillée

--- MONSCRIPT1.py
+++ MONSCRIPT1.py
@@ -1,3 +1,5 @@
+LK
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -10,6 +12,28 @@ def MONSCRIPT1(fichier):
     for id in data:
         plt.plot(data.sent_at,data.temp)
     plt.show()
-            
+
 
 data1=data[data['id']==1]
+
+09/11/20
+JV
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+
+os.chdir("C:/EIVP")
+
+data=pd.read_csv("EIVP_KM.csv",sep='/;')
+
+sent_at,temp=data['sent_at'],data['temp']
+
+plt.plot(sent_at,temps,"+",label="temperature en fonction du temps")
+plt.xlabel ("date",fontsize=5)
+plt.ylabel ("temperature",fontsize=8)
+plt.xticks(rotation='vertical')
+plt.legend()
+
+plt.show()
