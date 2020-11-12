3e05b63d52ba7514378f91a6046eeaf01f9f1ab8
LaetitiaKieffer <laetikief@gmail.com>
2020-11-12 17:03:46 +0100

    2ème essai

--- MONSCRIPT1.py
+++ MONSCRIPT1.py
@@ -1,23 +1,21 @@
-LK
-
+#LK
+#import des bibliothèques
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
-#'/Users/laetitia/Desktop/Projet_informatique/donnees_projet.csv'
+data =pd.read_csv('/Users/laetitia/Desktop/Projet_informatique/donnees_projet.csv',delimiter=';')
 
 def MONSCRIPT1(fichier):
     data=pd.read_csv(fichier,delimiter=';')
-    for id in data:
+    for i in data['id'].items():
         plt.plot(data.sent_at,data.temp)
     plt.show()
 
-
 data1=data[data['id']==1]
 
-09/11/20
-JV
+#09/11/20
+#JV
 
 import numpy as np
 import pandas as pd
@@ -30,7 +28,7 @@ data=pd.read_csv("EIVP_KM.csv",sep='/;')
 
 sent_at,temp=data['sent_at'],data['temp']
 
-plt.plot(sent_at,temps,"+",label="temperature en fonction du temps")
+plt.plot(sent_at,temp,"+",label="temperature en fonction du temps")
 plt.xlabel ("date",fontsize=5)
 plt.ylabel ("temperature",fontsize=8)
 plt.xticks(rotation='vertical')
