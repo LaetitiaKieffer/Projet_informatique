9f6e12d45d70cc80617fac6c309d6b6109b578fc
LaetitiaKieffer <laetikief@gmail.com>
2020-11-15 15:14:27 +0100

    essaye les deux dernières lignes pour la représentation du capteur 1

--- MONSCRIPT2.py
+++ MONSCRIPT2.py
@@ -6,20 +6,76 @@ import matplotlib.pyplot as plt
 #import tabeau excel
 data =pd.read_csv('/Users/laetitia/Desktop/Projet_informatique_sujet/donnees_projet.csv',delimiter=';')
 
-
 def MAX(fichier):
+    maxs=[]
     data=pd.read_csv(fichier,delimiter=';')
     a=0
     for index,row in data.iterrows():
         if row['temp']>a:
             a=row['temp']
-    return a
+    L.append(a)
+
+
+def MAX2(fichier):
+    data=pd.read_csv(fichier,delimiter=';')
+    a=0
+    maxs=[]
+    for i in range (1,7):
+        datainter=data.loc[data['id']==i]
+        for index,row in data.iterrows():
+            if row['temp']>a:
+                a=row['temp']
+        maxs.append(a)
+    return maxs
 
 
 def MIN(fichier):
     data=pd.read_csv(fichier,delimiter=';')
     a=30
+    mins=[]
+    for i in range (1,7):
+
     for index,row in data.iterrows():
         if row['temp']<a:
             a=row['temp']
     return a
+
+def MOYENNE(fichier):
+    data=pd.read_csv(fichier,delimiter=';')
+    moy=0
+    data=data.loc[data['id']==1]
+    for index,row in data.iterrows():
+        moy+=row['temp']
+    moy=moy/data.shape[0]
+    return moy
+
+def VARIANCE(fichier):
+    data=pd.read_csv(fichier,delimiter=';')
+    var=0
+    moy=MOYENNE(fichier)
+    data=data.loc[data['id']==1]
+    for index,row in data.iterrows():
+        var+=(row['temp']-moy)**2
+    var=var/data.shape[0]
+    return var
+
+def MEDIANE(fichier):
+    data=pd.read_csv(fichier,delimiter=';')
+    L=[]
+    med=0
+    data=data.loc[data['id']==1]
+    for index,row in data.iterrows():
+        if row['temp'] not in L:
+            L.append(row['temp'])
+    for j in L:
+        med+=j
+    med=med/len(L)
+    return med
+
+#représentation graphique du capteur 1
+data_capteur_1=data.loc[data['id']==1]
+plt.plot(data_capteur_1['sent_at'],data_capteur_1['temp'])
+
+
+
+
