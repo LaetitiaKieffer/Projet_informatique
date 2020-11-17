8956352c71802db1f41b467eedf54fa05fa546c0
Joséphine Venisse <josephine.venisse@eivp-paris.fr>
2020-11-17 12:32:45 +0100

    afficher les courbes

--- MONSCRIPT1.py
+++ MONSCRIPT1.py
@@ -6,64 +6,34 @@ import matplotlib.pyplot as plt
 
 data =pd.read_csv('/Users/laetitia/Desktop/Projet_informatique_sujet/donnees_projet.csv',delimiter=';')
 
-def GRAPH(fichier):
-    data=pd.read_csv(fichier,delimiter=';')
-    for i in data['id'].items():
-        plt.plot(data['sent_at'],data['temp'])
-    plt.show()
 
-def GRAPH2(fichier):
-    data=pd.read_csv(fichier,delimiter=';')
-    dataconst=data.loc[data['id']==1]
-    i=1
-    while i < 7:
-        datainter=data.loc[data['id']==i]
-        i=i+1
-        return datainter
-        #plt.plot(dataconst['sent_at'],datainter['temp'])
-    #plt.show
-
-def GRAPH3(fichier):
-    data=pd.read_csv(fichier,delimiter=';')
-    dataconst=data.loc[data['id']==1]
-    for i in range (1,7):
-        data=data.loc[data['id']==i]
-        #return dataconst['sent_at']
-        #return data['temp'].astype(object)
-        plt.plot(dataconst['sent_at'],data['temp'].astype(object))
-plt.show
-
-
-
-
-def essai(fichier):
-    data=pd.read_csv(fichier,delimiter=';')
-    L=[]
-    for i in data['id'].items():
-        L.append(data.temp)
-    return L
-
-
-data1=data[data['id']==1]
-
-#09/11/20
-#JV
-
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
+import matplotlib
 import os
+from datetime import datetime
+
 
 os.chdir("C:/EIVP")
+fichier="EIVP_KM.csv"
+data=pd.read_csv(fichier,delimiter=';')
 
-data=pd.read_csv("EIVP_KM.csv",sep='/;')
+def convertion(capteur):
+    L=len(data[data.id==capteur])
+    Liste=[]
+    for k in range(L):
+        Liste.append(datetime.strptime(data.sent_at.loc[k+data[data.id==capteur].index[0]],"%Y-%m-%d %H:%M:%S+02:00"))
+    return Liste
+#print (convertion(1))
 
-sent_at,temp=data['sent_at'],data['temp']
 
-plt.plot(sent_at,temp,"+",label="temperature en fonction du temps")
-plt.xlabel ("date",fontsize=5)
-plt.ylabel ("temperature",fontsize=8)
-plt.xticks(rotation='vertical')
-plt.legend()
+def affichercourbes(caracteristique,capteurr):
+    data_capteur=data.loc[data['id']==capteurr]
+    plt.plot_date(matplotlib.dates.date2num(convertion(capteurr)),data_capteur[caracteristique],label="ligne -",linewidth=1)
+    plt.title (caracteristique+" as a function of time")
+    plt.xlabel ("date",fontsize=9)
+    plt.ylabel ("temperature",fontsize=9)
+    plt.xticks(rotation='vertical')
+    plt.show()
 
-plt.show()
+def affichertteslescourbes(caracteristique):
+    for k in range (1,7):
+        affichercourbes(caracteristique,k)
