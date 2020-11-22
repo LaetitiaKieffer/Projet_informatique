11376f8a12abaa5c97fdd0cee976121f5aa7aacf
Joséphine Venisse <josephine.venisse@eivp-paris.fr>
2020-11-22 14:07:41 +0100

    fct min en plus

--- MONSCRIPT2.py
+++ MONSCRIPT2.py
@@ -2,80 +2,97 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
+
+os.chdir("C:/EIVP")
+fichier="EIVP_KM.csv"
+data=pd.read_csv(fichier,delimiter=';')
 
 #import tabeau excel
-data =pd.read_csv('/Users/laetitia/Desktop/Projet_informatique_sujet/donnees_projet.csv',delimiter=';')
-
-def MAX(fichier):
-    maxs=[]
-    data=pd.read_csv(fichier,delimiter=';')
-    a=0
-    for index,row in data.iterrows():
-        if row['temp']>a:
-            a=row['temp']
-    L.append(a)
-
-
-def MAX2(fichier):
-    data=pd.read_csv(fichier,delimiter=';')
-    a=0
-    maxs=[]
-    for i in range (1,7):
-        datainter=data.loc[data['id']==i]
-        for index,row in data.iterrows():
-            if row['temp']>a:
-                a=row['temp']
-        maxs.append(a)
-    return maxs
-
-
-def MIN(fichier):
-    data=pd.read_csv(fichier,delimiter=';')
-    a=30
-    mins=[]
-    for i in range (1,7):
-
-    for index,row in data.iterrows():
-        if row['temp']<a:
-            a=row['temp']
-    return a
-
-def MOYENNE(fichier):
-    data=pd.read_csv(fichier,delimiter=';')
-    moy=0
-    data=data.loc[data['id']==1]
-    for index,row in data.iterrows():
-        moy+=row['temp']
-    moy=moy/data.shape[0]
-    return moy
-
-def VARIANCE(fichier):
-    data=pd.read_csv(fichier,delimiter=';')
-    var=0
-    moy=MOYENNE(fichier)
-    data=data.loc[data['id']==1]
-    for index,row in data.iterrows():
-        var+=(row['temp']-moy)**2
-    var=var/data.shape[0]
-    return var
-
-def MEDIANE(fichier):
-    data=pd.read_csv(fichier,delimiter=';')
-    L=[]
-    med=0
-    data=data.loc[data['id']==1]
-    for index,row in data.iterrows():
-        if row['temp'] not in L:
-            L.append(row['temp'])
-    for j in L:
-        med+=j
-    med=med/len(L)
-    return med
+# data =pd.read_csv('/Users/laetitia/Desktop/Projet_informatique_sujet/donnees_projet.csv',delimiter=';')
+# #
+# # def MAX(fichier):
+# #     maxs=[]
+# #     data=pd.read_csv(fichier,delimiter=';')
+# #     a=0
+# #     for index,row in data.iterrows():
+# #         if row['temp']>a:
+# #             a=row['temp']
+#NON #     L.append(a)
+# #
+#
+# def MAX2(fichier):
+#     data=pd.read_csv(fichier,delimiter=';')
+#     a=0
+#     maxs=[]
+#     for i in range (1,7):
+#         datainter=data.loc[data['id']==i]
+#         for index,row in data.iterrows():
+#             if row['temp']>a:
+#                 a=row['temp']
+#         maxs.append(a)
+#     return maxs
+#
+#
+# def MIN(fichier):
+#     data=pd.read_csv(fichier,delimiter=';')
+#     a=30
+#     mins=[]
+#     for i in range (1,7):
+#
+#     for index,row in data.iterrows():
+#         if row['temp']<a:
+#             a=row['temp']
+#     return a
+#
+# def MOYENNE(fichier):
+#     data=pd.read_csv(fichier,delimiter=';')
+#     moy=0
+#     data=data.loc[data['id']==1]
+#     for index,row in data.iterrows():
+#         moy+=row['temp']
+#     moy=moy/data.shape[0]
+#     return moy
+#
+# def VARIANCE(fichier):
+#     data=pd.read_csv(fichier,delimiter=';')
+#     var=0
+#     moy=MOYENNE(fichier)
+#     data=data.loc[data['id']==1]
+#     for index,row in data.iterrows():
+#         var+=(row['temp']-moy)**2
+#     var=var/data.shape[0]
+#     return var
+#
+# def MEDIANE(fichier):
+#     data=pd.read_csv(fichier,delimiter=';')
+#     L=[]
+#     med=0
+#     data=data.loc[data['id']==1]
+#     for index,row in data.iterrows():
+#         if row['temp'] not in L:
+#             L.append(row['temp'])
+#     for j in L:
+#         med+=j
+#     med=med/len(L)
+#     return med
 
 #représentation graphique du capteur 1
-data_capteur_1=data.loc[data['id']==1]
+data_capteur_1=data.loc[data['id']==4]
 plt.plot(data_capteur_1['sent_at'],data_capteur_1['temp'])
 
+data_capteur_1=data.loc[data['id']==2]
+plt.plot(data_capteur_1['sent_at'],data_capteur_1['temp'])
 
+data_capteur_1=data.loc[data['id']==3]
+plt.plot(data_capteur_1['sent_at'],data_capteur_1['temp'])
 
+plt.show()
 
+def MIN(fichier,caractéristique):
+    data=pd.read_csv(fichier,delimiter=';')
+    a=30
+    for index,row in data.iterrows():
+        if row[caractéristique]<a:
+            a=row[caractéristique]
+    return a
