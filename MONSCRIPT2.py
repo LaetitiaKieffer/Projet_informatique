6893a34252483357d2b88b460433109db20e0aad
Joséphine Venisse <josephine.venisse@eivp-paris.fr>
2020-11-22 16:45:42 +0100

    fct min max .. pr chaque capteur

--- MONSCRIPT2.py
+++ MONSCRIPT2.py
@@ -11,48 +11,48 @@ data=pd.read_csv(fichier,delimiter=';')
 #import tabeau excel
 data =pd.read_csv('/Users/laetitia/Desktop/Projet_informatique_sujet/donnees_projet_informatique.csv',delimiter=';')
 
-def MAX(fichier,caractéristique):
+def MAX(fichier,caractéristique,capteur):
     data=pd.read_csv(fichier,delimiter=';')
-    data=data.loc[data['id']==1]
+    data=data.loc[data['id']==capteur]
     a=0
     for index,row in data.iterrows():
         if row[caractéristique]>a:
             a=row[caractéristique]
     return a
 
-def MIN(fichier,caractéristique):
+def MIN(fichier,caractéristique,capteur):
     data=pd.read_csv(fichier,delimiter=';')
-    data=data.loc[data['id']==1]
+    data=data.loc[data['id']==capteur]
     a=30
     for k in range (len(data)):
         if data.loc[k,caractéristique]<a:
             a=data[k,caractéristique]
     return a
 
-def MOYENNE(fichier,caractéristique):
+def MOYENNE(fichier,caractéristique,capteur):
     data=pd.read_csv(fichier,delimiter=';')
     moy=0
-    data=data.loc[data['id']==1]
+    data=data.loc[data['id']==capteur]
     for index,row in data.iterrows():
         moy+=row[caractéristique]
     moy=moy/data.shape[0]
     return moy
 
-def VARIANCE(fichier,caractéristique):
+def VARIANCE(fichier,caractéristique,capteur):
     data=pd.read_csv(fichier,delimiter=';')
     var=0
     moy=MOYENNE(fichier,caractéristique)
-    data=data.loc[data['id']==1]
+    data=data.loc[data['id']==capteur]
     for index,row in data.iterrows():
         var+=(row[caractéristique]-moy)**2
     var=var/data.shape[0]
     return var
 
-def MEDIANE(fichier,caractéristique):
+def MEDIANE(fichier,caractéristique,capteur):
     data=pd.read_csv(fichier,delimiter=';')
     L=[]
     med=0
-    data=data.loc[data['id']==1]
+    data=data.loc[data['id']==capteur]
     for index,row in data.iterrows():
         if row[caractéristique] not in L:
             L.append(row[caractéristique])
@@ -60,5 +60,3 @@ def MEDIANE(fichier,caractéristique):
         med+=j
     med=med/len(L)
     return med
-
-nn
