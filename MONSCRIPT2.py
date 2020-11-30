4b3f54ec3870f4535f7f57b7dfd0cc46399cf105
Joséphine Venisse <josephine.venisse@eivp-paris.fr>
2020-11-30 18:48:33 +0100

    affichercourbe termine

--- MONSCRIPT2.py
+++ MONSCRIPT2.py
@@ -4,6 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib
 import os
+from datetime import datetime
 
 #import tableau excel Joséphine
 os.chdir("C:/EIVP")
@@ -11,8 +12,8 @@ fichier="EIVP_KM.csv"
 data=pd.read_csv(fichier,delimiter=';')
 
 #import tabeau excel Laetitia
-fichier='/Users/laetitia/Desktop/Projet_informatique_sujet/donnees_projet_informatique.csv'
-data =pd.read_csv('/Users/laetitia/Desktop/Projet_informatique_sujet/donnees_projet_informatique.csv',delimiter=';')
+# fichier='/Users/laetitia/Desktop/Projet_informatique_sujet/donnees_projet_informatique.csv'
+# data =pd.read_csv('/Users/laetitia/Desktop/Projet_informatique_sujet/donnees_projet_informatique.csv',delimiter=';')
 
 def MAX(fichier,caractéristique,capteur):
     data =pd.read_csv(fichier,delimiter=';')
@@ -70,6 +71,7 @@ def convertion(capteur):
     return Liste
 
 def afficher_courbes(caracteristique,capteurr):
+    fichier="EIVP_KM.csv"
     data_capteur=data.loc[data['id']==capteurr]
     plt.plot_date(matplotlib.dates.date2num(convertion(capteurr)),data_capteur[caracteristique],linestyle="-",marker=None)
     plt.title (caracteristique+" as a function of time")
@@ -77,10 +79,20 @@ def afficher_courbes(caracteristique,capteurr):
     plt.ylabel ("temperature",fontsize=9)
     plt.xticks(rotation='vertical')
     LegendeVariance=matplotlib.patches.Rectangle((0,0),0,0,color='white')
-    l1=plt.axhline(y=MIN(fichier,caracteristique,capteurr),label='minimum',color='mediumvioletred')
-    l2=plt.axhline(y=MAX(fichier,caracteristique,capteurr),label='maximum',color='pink')
-    l3=plt.axhline(y=MOYENNE(fichier,caracteristique,capteurr),label='maximum',color='magenta')
-    l4=plt.axhline(y=MEDIANE(fichier,caracteristique,capteurr),label='maximum',color='deeppink')
-    plt.legend([l1,l2,l3,l4,LegendeVariance], ['minimum', 'maximum', 'moyenne','médiane','variance='+str(VARIANCE(fichier,caracteristique,capteurr))],loc ='upper right',frameon = True, title = 'Legende')
-    plt.show()
 
+    l1=plt.axhline(y=MIN(fichier,caracteristique,capteurr),label='minimum',color='mediumvioletred')
+    plt.text((datetime.strptime(data.sent_at.loc[data[data.id==capteurr].index[capteurr*200]],"%Y-%m-%d %H:%M:%S+02:00")),MIN(fichier,caracteristique,capteurr),'capteur '+str(capteurr),fontsize = 7,fontweight = 'bold', rotation = 70)
+
+    l2=plt.axhline(y=MAX(fichier,caracteristique,capteurr),label='maximum',color='pink')
+    plt.text((datetime.strptime(data.sent_at.loc[data[data.id==capteurr].index[capteurr*200]],"%Y-%m-%d %H:%M:%S+02:00")),MAX(fichier,caracteristique,capteurr),'capteur '+str(capteurr),fontsize = 7,fontweight = 'bold', rotation = 70)
+
+    l3=plt.axhline(y=MOYENNE(fichier,caracteristique,capteurr),label='maximum',color='magenta')
+    plt.text((datetime.strptime(data.sent_at.loc[data[data.id==capteurr].index[capteurr*200]],"%Y-%m-%d %H:%M:%S+02:00")),MOYENNE(fichier,caracteristique,capteurr),'capteur '+str(capteurr),fontsize=7,fontweight='bold', rotation=70)
+
+    l4=plt.axhline(y=MEDIANE(fichier,caracteristique,capteurr),label='maximum',color='deeppink')
+    plt.text((datetime.strptime(data.sent_at.loc[data[data.id==capteurr].index[capteurr*200]],"%Y-%m-%d %H:%M:%S+02:00")),MEDIANE(fichier,caracteristique,capteurr),'capteur '+str(capteurr),fontsize = 7,fontweight = 'bold', rotation = 70)
+
+    plt.legend([l1,l2,l3,l4,LegendeVariance], ['minimum','maximum','moyenne','médiane','variance='+str(VARIANCE(fichier,caracteristique,capteurr))],loc ='upper right',frameon = True, title = 'Legende')
+
+
+    plt.show()
